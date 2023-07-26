770ea818c739a982b10c23691c0fd023997c30e4
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 14:27:39 -0500

    Recuperar datos a editar

--- libreria/views.py
+++ libreria/views.py
@@ -20,8 +20,10 @@ def crear(request):
         formulario.save()
         return redirect('libros')
     return render(request, "libros/crear.html", {'formulario': formulario})
-def editar(request):
-    return render(request, "libros/editar.html")
+def editar(request, id):
+    libro= Libro.objects.get(id=id)
+    formulario = LibroForm(request.POST or None, request.FILES or None, instance=libro)
+    return render(request, "libros/editar.html", {'formulario': formulario})
 def eliminar(request, id):
     libro = Libro.objects.get(id=id)
     libro.delete()
