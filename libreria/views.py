bedec7a7a235561ec13eab7e2111fa5eb0dab21b
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 14:03:39 -0500

    guardar libros

--- libreria/views.py
+++ libreria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 from .models import Libro
@@ -15,7 +15,10 @@ def libros(request):
     libros = Libro.objects.all()
     return render(request, "libros/index.html", {'libros': libros})
 def crear(request):
-    formulario = LibroForm(request.POST or None)
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
     return render(request, "libros/crear.html", {'formulario': formulario})
 def editar(request):
     return render(request, "libros/editar.html")
