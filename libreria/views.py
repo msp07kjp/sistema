e1435507f094884e3f4e94c70ce5f535524600e1
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 10:37:56 -0500

    btn agregar libro

--- libreria/views.py
+++ libreria/views.py
@@ -5,5 +5,9 @@ def inicio(request):
     return render(request, 'paginas/inicio.html')
 def nosotros(request):
     return render(request, "paginas/nosotros.html")
+
+
 def libros(request):
     return render(request, "libros/index.html")
+def crear(request):
+    return render(request, "libros/crear.html")
