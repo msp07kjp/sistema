2c9c14dc9d7a50e78b1d520b1a5e206a7a677123
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 12:10:37 -0500

    consultando libros de la bd

--- libreria/views.py
+++ libreria/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Libro
+
 # Create your views here.
 def inicio(request):
     return render(request, 'paginas/inicio.html')
@@ -8,7 +11,8 @@ def nosotros(request):
 
 
 def libros(request):
-    return render(request, "libros/index.html")
+    libros = Libro.objects.all()
+    return render(request, "libros/index.html", {'libros': libros})
 def crear(request):
     return render(request, "libros/crear.html")
 def editar(request):
