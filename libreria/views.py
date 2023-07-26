81a63c9939fb918e92a975a9796fb2d0341c27df
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 13:56:40 -0500

    hacer formulario de creacion de libros

--- libreria/views.py
+++ libreria/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Libro
+from .forms import LibroForm
 
 # Create your views here.
 def inicio(request):
@@ -14,6 +15,7 @@ def libros(request):
     libros = Libro.objects.all()
     return render(request, "libros/index.html", {'libros': libros})
 def crear(request):
-    return render(request, "libros/crear.html")
+    formulario = LibroForm(request.POST or None)
+    return render(request, "libros/crear.html", {'formulario': formulario})
 def editar(request):
     return render(request, "libros/editar.html")
