096668a9a0684f5d8cf031d659c706effc27c251
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 10:30:21 -0500

    menu y inicio

--- libreria/views.py
+++ libreria/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
-    return HttpResponse("<h1>Hola mundo desde Django</h1>")
+    return render(request, 'paginas/inicio.html')
 def nosotros(request):
     return render(request, "paginas/nosotros.html")
+def libros(request):
+    return render(request, "libros/index.html")
