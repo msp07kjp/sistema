8476c5168268419daaee2f802b1b9102e81a6468
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 11:16:46 -0500

    conexion bd y modelo libro(migradoa bd)

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen',null=True)
+    descripcion = models.TextField(verbose_name='Descripcion',null=True)
